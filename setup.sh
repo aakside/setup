1186aeac0a400fd4faf5fb0e4609a103686ae974
Alvin Khaled <akhaled@rocketfuel.com>
2016-01-04 23:07:03 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir ~/Dropbox
+ln -s ~/Dropbox/Documents
+ln -s ~/Dropbox/Music
+# TODO: Copy SSH keys?
+# TODO: Prompt user to install Dropbox and use ~/Dropbox as installation dir.
+
+# TODO: Install typefaces
+
+cp ./.vim ~/.vim
+cp ./.vimrc ~/.vimrc
+
