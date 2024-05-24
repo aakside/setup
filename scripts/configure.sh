c911c149125cda77c42b484aec16e7a9ca63f39a
Alvin Ali Khaled <aakside@gmail.com>
2024-05-24 17:44:13 -0400

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -99,11 +99,11 @@ if [ "$DISTRO" == "darwin" ]; then
   cat $ASSETS/.macos_bash_aliases >> ~/.bash_aliases
   defaults write com.apple.desktopservices DSDontWriteNetworkStores true
   xcode-select --install && \
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
     brew install ack coreutils direnv ffmpeg flac gdbm gettext glib gnutls \
     gradle jpeg lame libogg libpng libtiff libvorbis libvpx libyaml \
     openjpeg openssl pcre readline sbt sqlite webp wget x264 x265 xvid && \
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/alvinkhaled/.zprofile && \
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile && \
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
